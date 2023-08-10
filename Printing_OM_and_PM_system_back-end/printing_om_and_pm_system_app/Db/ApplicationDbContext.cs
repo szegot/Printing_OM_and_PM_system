fbffdccb6711b04c2f04a58000b324c61c827fbf
Tibor_Szego <szego.t@gmail.com>
2023-08-10 22:02:40 +0200

    update database

--- Printing_OM_and_PM_system_back-end/printing_om_and_pm_system_app/Db/ApplicationDbContext.cs
+++ Printing_OM_and_PM_system_back-end/printing_om_and_pm_system_app/Db/ApplicationDbContext.cs
@@ -9,8 +9,17 @@ namespace printing_om_and_pm_system_app.Db
 		{
 			
 		}
-        
+
         public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemService> ItemServices { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Machine> Machines { get; set; }
+        public DbSet<Process> Processes { get; set; }
+        public DbSet<ProcessService> ProcessServices { get; set; }
 
     }
 }
--- Printing_OM_and_PM_system_back-end/printing_om_and_pm_system_app/Models/User.cs
+++ Printing_OM_and_PM_system_back-end/printing_om_and_pm_system_app/Models/User.cs
@@ -2,19 +2,41 @@
 {
     public class User
     {
-        public int Id { get; set; }
+        [Key]
+        public int User_ID { get; set; }
+
         [Required(ErrorMessage = "Username is required.")]
+        [StringLength(maximumLength: 255, MinimumLength = 1)]
         public string UserName { get; set; } = string.Empty;
-        [Required(ErrorMessage = "Name is required.")]
-        public string Name { get; set; } = string.Empty;
-        [Required(ErrorMessage = "Password is required.")]
-        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(maximumLength: 50, MinimumLength = 1)]
+        public string First_Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(maximumLength: 50, MinimumLength = 1)]
+        public string Last_Name { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "Email is required.")]
+        [StringLength(maximumLength: 255, MinimumLength = 1)]
         public string Email { get; set; } = string.Empty;
-        [Required(ErrorMessage = "Billing Adress is required.")]
-        public string Billing_Adress { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(maximumLength: 64, MinimumLength = 1)]
+        public string Password { get; set; } = string.Empty;
+
+        [MaxLength(255)]
+        public string Billing_Address { get; set; } = string.Empty;
+
+        [MaxLength(255)]
+        public string Billing_City { get; set; } = string.Empty;
+
+        [MaxLength(20)]
+        public int Billing_Zipcode { get; set; }
+
         public bool IsAdmin { get; set; } = false;
-        
+
+        public ICollection<Order> Orders { get; set; }
     }
 }
 
